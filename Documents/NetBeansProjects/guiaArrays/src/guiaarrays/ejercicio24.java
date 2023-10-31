81933cfa6b12ca69b0a0db4bece1f478d523667e
Hamilton <fp23010@ues.edu.sv>
2023-10-30 18:32:27 -0600

    24 actualizado

--- Documents/NetBeansProjects/guiaArrays/src/guiaarrays/ejercicio24.java
+++ Documents/NetBeansProjects/guiaArrays/src/guiaarrays/ejercicio24.java
@@ -7,19 +7,30 @@ public class ejercicio24 {
     public static void main(String[] args) {
         int[] arrayOriginal = {1, 2, 2, 3, 4, 4, 5};
         int[] arraySinDuplicados = new int[arrayOriginal.length];
-        
-        for(int i=0; i<arraySinDuplicados.length-1; i++){
-            for(int j=0; j<arrayOriginal.length-1; j++){
-                if(arraySinDuplicados[i]==arrayOriginal[j]){
-                    
-                }else{
-                    arraySinDuplicados[i]=arrayOriginal[j];
+        int j,top=0;
+        boolean repetido;
+        for(int i=0; i<arrayOriginal.length; i++){
+            repetido=false;
+            j=0;
+            while(!repetido && j<top){
+                if(arrayOriginal[i] == arraySinDuplicados[j]){
+                    repetido=true;
                 }
+                j++;
+            }
+            if(!repetido){
+                arraySinDuplicados[top]=arrayOriginal[i];
+                top++;
             }
         }
-        System.out.println("Nuevo array");
-        for(int b: arraySinDuplicados){
-            System.out.println(b);
+        System.out.println("Array original:");
+        for(int i=0; i<arrayOriginal.length; i++){
+            System.out.print(arrayOriginal[i]+" ");
+        }
+        System.out.println("");
+        System.out.println("Arra sin duplicados:");
+        for(int i=0; i<top; i++){
+            System.out.print(arraySinDuplicados[i]+" ");
         }
     } 
 }
